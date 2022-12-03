0052cb98c7c4f41fee544d7f9f3b81124d6f4c09
NarutoBratishka <katorabian@gmail.com>
2022-12-04 01:23:12 +0700

    refactor: change: replace syntetics with viewBinding in adapters

---
+++ app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.cryptoapp.presentation.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoapp.databinding.ItemCoinInfoBinding
+
+class CoinInfoViewHolder(
+    val itemBinding: ItemCoinInfoBinding
+) : RecyclerView.ViewHolder(itemBinding.root)
