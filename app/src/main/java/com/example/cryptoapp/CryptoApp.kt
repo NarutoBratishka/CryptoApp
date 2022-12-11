2e882e4e3a24ac06b11beb9ff241e12629b26984
NarutoBratishka <katorabian@gmail.com>
2022-12-11 12:26:46 +0700

    refactor: add: custom WorkerFactory

--- app/src/main/java/com/example/cryptoapp/CryptoApp.kt
+++ app/src/main/java/com/example/cryptoapp/CryptoApp.kt
@@ -1,12 +1,28 @@
 package com.example.cryptoapp
 
 import android.app.Application
+import androidx.work.Configuration
+import com.example.cryptoapp.data.database.AppDatabase
+import com.example.cryptoapp.data.mapper.CoinMapper
+import com.example.cryptoapp.data.network.ApiFactory
+import com.example.cryptoapp.data.workers.RefreshDataWorkerFactory
 import com.example.cryptoapp.di.DaggerApplicationComponent
 
-class CryptoApp: Application() {
+class CryptoApp: Application(), Configuration.Provider {
 
     val component by lazy {
         DaggerApplicationComponent.factory()
             .create(this)
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(
+                RefreshDataWorkerFactory(
+                    AppDatabase.getInstance(this).coinInfoDao(),
+                    ApiFactory.apiService,
+                    CoinMapper()
+                )
+            ).build()
     }
 }
