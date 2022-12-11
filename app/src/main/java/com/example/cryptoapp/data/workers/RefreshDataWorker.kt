8b0640d293c0c859e77a5e6a6037f4543974fde0
NarutoBratishka <katorabian@gmail.com>
2022-12-11 20:52:18 +0700

    refactor: change: inject arguments to WorkerFactory

--- app/src/main/java/com/example/cryptoapp/data/workers/RefreshDataWorker.kt
+++ app/src/main/java/com/example/cryptoapp/data/workers/RefreshDataWorker.kt
@@ -2,12 +2,11 @@ package com.example.cryptoapp.data.workers
 
 import android.content.Context
 import androidx.work.*
-import com.example.cryptoapp.data.database.AppDatabase
 import com.example.cryptoapp.data.database.CoinInfoDao
 import com.example.cryptoapp.data.mapper.CoinMapper
-import com.example.cryptoapp.data.network.ApiFactory
 import com.example.cryptoapp.data.network.ApiService
 import kotlinx.coroutines.delay
+import javax.inject.Inject
 
 class RefreshDataWorker(
     context: Context,
@@ -35,6 +34,26 @@ class RefreshDataWorker(
         const val NAME = "RefreshDataWorker"
         fun makeRequest(): OneTimeWorkRequest {
             return OneTimeWorkRequestBuilder<RefreshDataWorker>().build()
+        }
+    }
+
+    class Factory @Inject constructor(
+        private val coinInfoDao: CoinInfoDao,
+        private val apiService: ApiService,
+        private val mapper: CoinMapper
+    ): ChildWorkerFactory {
+
+        override fun create(
+            context: Context,
+            workerParameters: WorkerParameters
+        ): ListenableWorker {
+            return RefreshDataWorker(
+                context,
+                workerParameters,
+                coinInfoDao,
+                apiService,
+                mapper
+            )
         }
     }
 }
--- app/src/main/java/com/example/cryptoapp/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/cryptoapp/di/ApplicationComponent.kt
@@ -2,15 +2,13 @@ package com.example.cryptoapp.di
 
 import android.app.Application
 import com.example.cryptoapp.CryptoApp
-import com.example.cryptoapp.presentation.CoinDetailActivity
 import com.example.cryptoapp.presentation.CoinDetailFragment
 import com.example.cryptoapp.presentation.CoinPriceListActivity
 import dagger.BindsInstance
 import dagger.Component
-import javax.inject.Singleton
 
 @ApplicationScope
-@Component(modules = [DataModule::class, ViewModelModule::class])
+@Component(modules = [DataModule::class, ViewModelModule::class, WorkerModule::class])
 interface ApplicationComponent {
 
     fun inject(application: CryptoApp)
